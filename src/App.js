fdd376de11ed15f1cb635c8c8e76bdb7db3be81e
jailsonsf <jailsonsoares002@gmail.com>
2019-10-02 18:22:39 -0300

    Add login screen :rocket:

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import './App.css';
+
+import logo from './assets/logo.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Hello World</p>
-      </header>
+    <div className="container">
+      <img src={logo} alt="AirCnC"/>
+
+      <div className="content">
+        <p>
+          Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa
+        </p>
+
+        <form>
+          <label htmlFor="email">E-Mail *</label>
+          <input 
+            type="email" 
+            id="email" 
+            placeholder="Seu melhor e-mail"
+          />
+          <button className="btn" type="submit">Entrar</button>
+        </form>
+
+      </div>
     </div>
   );
 }
